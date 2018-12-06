508ce59f39e27099c7d2bc2a9e0c04cffa418da1
Denis Augsburger <d.augsbi@gmail.com>
2018-12-06 16:34:31 +0100

    Add apollo with basic configuration

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+type Config = {
+  githubToken: string;
+};
+
+export const config: Config = {
+  // Use your created githubToken with scope (repo, user i.e.)
+  githubToken: null
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+import { config } from "./config";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Create the client with the needed configuration
+const client = new ApolloClient({
+  // By default, this client will send queries to the
+  //  `/graphql` endpoint on the same host
+  // Pass the configuration option { uri: YOUR_GRAPHQL_API_URL } to the `HttpLink` to connect
+  // to a different host
+  link: new HttpLink({
+    // Define your backend (our github api) with the corresponding key
+    uri: "https://api.github.com/graphql",
+    headers: {
+      authorization: `Bearer ${config.githubToken}`
+    }
+  }),
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
